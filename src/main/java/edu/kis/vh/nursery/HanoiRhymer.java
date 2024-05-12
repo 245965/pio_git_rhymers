d4c5654914b4f241f5f94860d946f077e41869e2
245965 <245965@edu.p.lodz.pl>
2024-05-12 16:40:34 +0200

    2.Formatowanie

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,7 +5,7 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     int totalRejected = 0;
 
     public int reportRejected() {
-        return this.totalRejected;
+        return totalRejected;
     }
 
     public void countIn(int in) {
