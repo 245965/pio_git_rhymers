635b240603f5fd70b75915c9b0b598f0efbce7cb
Michal Strozak <245931@edu.p.lodz.pl>
2024-05-19 22:10:04 +0200

    7. Ustawienie modyfikatora final przy atrybutach klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,10 +2,10 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public int CAPACITY = 12;
-    public int EMPTY = -1;
+    public static final int CAPACITY = 12;
+    public static final int EMPTY = -1;
 
-    private int[] numbers = new int[CAPACITY];
+    private final int[] numbers = new int[CAPACITY];
 
     public int total = EMPTY;
 
